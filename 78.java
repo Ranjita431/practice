712f224add8a85831bc256dfbfdd0703e322acc3
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-02-06 22:55:43 +0545

    adding 78.java

---
+++ 78.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    int myNum = 5;        // myNum is an int
+    myNum = "Hello";      // Error: cannot assign a String to an int
+
+    String myText = "Hi"; // myText is a String
+    myText = 123;         // Error: cannot assign a number to a String
+
+    System.out.println(myNum);
+    System.out.println(myText);
+  }
+}
