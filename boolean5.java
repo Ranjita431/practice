ce7f8fd6b17a1fd70b3067e3f9bc6d760f4e728d
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-02-10 23:06:12 +0545

    adding boolean5.java

---
+++ boolean5.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 9;
+
+    boolean isGreater = x > y;
+
+    System.out.println(isGreater); // Outputs true
+  }
+}
