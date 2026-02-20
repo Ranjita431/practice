eb571005176932042c6e0733e33c45a99982205f
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-02-20 23:10:43 +0545

    adding rr48.java

---
+++ rr48.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+
+    for (int i = 0; i < 3; i++) {
+      System.out.println("Loop 1: " + i);
+    }
+
+    for (int i = 0; i < 2; i++) {
+      System.out.println("Loop 2: " + i);
+    }
+  }
+}
