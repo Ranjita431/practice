4bd170d02e43f68e9c01fe12c1b871601a8a2421
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-03-03 23:53:27 +0545

    adding de2.java

---
+++ de2.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    int age = 17;
+    System.out.println("Age: " + age);
+
+    if (age >= 18) {
+      System.out.println("Access granted");
+    } else {
+      System.out.println("Access denied");
+    }
+  }
+}
