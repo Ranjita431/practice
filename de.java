e9f1489a1c7e7bde2475eeb199d159f0001d603a
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-03-03 23:53:00 +0545

    adding de.java

---
+++ de.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 0;
+
+    System.out.println("Before division");  // Debug output
+
+    int result = x / y;  // Crashes
+
+    System.out.println("Result: " + result);  // Never runs
+  }
+}
