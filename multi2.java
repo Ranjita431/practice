a8e100af680e1623caf4698bfb810fd417326237
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-03-05 23:47:04 +0545

    adding multi2.java

---
+++ multi2.java
@@ -0,0 +1,17 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+  public static void main(String[] args) {
+    try {
+      FileOutputStream output = new FileOutputStream("filename.txt");
+      output.write("Hello".getBytes());
+      output.close();  // must close manually
+      System.out.println("Successfully wrote to the file.");
+    } catch (IOException e) {
+      System.out.println("Error writing file.");
+    }
+  }
+}
+
+      
