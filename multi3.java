d7cb03bbd30601790b5a03639da2114b01267e09
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-03-05 23:47:39 +0545

    adding multi3.java

---
+++ multi3.java
@@ -0,0 +1,17 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+  public static void main(String[] args) {
+    // resource is opened inside try()
+    try (FileOutputStream output = new FileOutputStream("filename.txt")) {
+      output.write("Hello".getBytes());
+      // no need to call close() here
+      System.out.println("Successfully wrote to the file.");
+    } catch (IOException e) {
+      System.out.println("Error writing file.");
+    }
+  }
+}
+
+     
